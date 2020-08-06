aad647c7e6a51936bbf0aea2dd9ec71d60705b70
SALSABELA KHAIRUNNISA <salsabela.khairunnisa@binus.ac.id>
2020-08-06 10:55:55 +0700

    Added LeaveType Repositories and CRUD and Added DataTables and Font Awesome Icons

---
+++ leave-management/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace leave_management.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<LeaveHistory> LeaveHistories { get; set; }
+        public DbSet<LeaveTypes> LeaveTypes { get; set; }
+        public DbSet<LeaveAllocation> LeaveAllocations { get; set; }
+        public object LeaveHistory { get; internal set; }
+    }
+}
